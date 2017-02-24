d6e4d0ab143b16750863dcac376ca256811766eb
glen newton <glen.newton@gmail.com>
2017-02-24 00:51:05 -0500

    Writer working & some writer tests

--- writer.go
+++ writer.go
@@ -8,20 +8,24 @@ import (
 )
 
 type Writer struct {
+	path        string
 	indexWriter *cdb.Writer
 	dataWriter  *cdb.Writer
+
+	indexCounter      int
+	indexCounterBytes []byte
+
+	dataCounter      int
+	dataCounterBytes []byte
 }
 
-var indexCounter = 0
-var indexCounterString = strconv.Itoa(indexCounter)
+const startCounter = 0
 
-var dataCounter = 0
-var dataCounterString = strconv.Itoa(dataCounter)
+var startCounterBytes = []byte(strconv.Itoa(startCounter))
 
 func Create(path string) (*Writer, error) {
 
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-
 		err := os.Mkdir(path, 0777)
 		if err != nil {
 			return nil, err
@@ -34,36 +38,89 @@ func Create(path string) (*Writer, error) {
 		}
 	}
 
+	writer := Writer{path: path, indexCounter: startCounter, dataCounter: startCounter, indexCounterBytes: startCounterBytes, dataCounterBytes: startCounterBytes}
+
 	//
 	// index
-	indexWriter, err := cdb.Create(path + pathSep + "index" + indexCounterString + ".cdb")
+	err := writer.nextIndex()
 	if err != nil {
 		return nil, err
 	}
-	indexCounter += 1
-	indexCounterString = strconv.Itoa(indexCounter)
 
 	//
 	// data
-	dataWriter, err := cdb.Create(path + pathSep + "data" + strconv.Itoa(dataCounter) + ".cdb")
+	err = writer.nextData()
 	if err != nil {
 		return nil, err
 	}
-	dataCounter += 1
-	dataCounterString = strconv.Itoa(dataCounter)
-	writer := Writer{indexWriter: indexWriter, dataWriter: dataWriter}
+
+	//writer := Writer{indexWriter: indexWriter, dataWriter: dataWriter}
 	return &writer, nil
 
 }
 
-var empty = []byte("")
-
 func (writer *Writer) Put(key, value []byte) error {
-	err := writer.indexWriter.Put(key, empty)
+	err := writer.indexWriter.Put(key, writer.dataCounterBytes)
+	if err != nil {
+		if err == cdb.ErrTooMuchData {
+			err = writer.nextIndex()
+		} else {
+			return err
+		}
+	}
+
+	err = writer.dataWriter.Put(key, value)
+	if err != nil {
+		if err == cdb.ErrTooMuchData {
+			err = writer.nextData()
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
+func (writer *Writer) nextData() error {
+	if writer.dataWriter != nil {
+		log.Println("Closing data")
+		err := writer.dataWriter.Close()
+		if err != nil {
+			return nil
+		}
+	}
+	log.Println("Opening new data")
+	dataWriter, err := newWriter(writer.path, "data", writer.dataCounter)
+	if err != nil {
+		return nil
+	}
+
+	writer.dataCounter += 1
+	writer.dataCounterBytes = []byte(strconv.Itoa(writer.dataCounter))
+	writer.dataWriter = dataWriter
+
+	return nil
+}
+
+func (writer *Writer) nextIndex() error {
+	if writer.indexWriter != nil {
+		log.Println("Closing index")
+		err := writer.indexWriter.Close()
+		if err != nil {
+			return nil
+		}
+	}
+
+	log.Println("Opening new index")
+
+	indexWriter, err := newWriter(writer.path, "index", writer.indexCounter)
 	if err != nil {
 		return err
 	}
-	return writer.dataWriter.Put(key, value)
+	writer.indexCounter += 1
+	writer.indexCounterBytes = []byte(strconv.Itoa(writer.indexCounter))
+	writer.indexWriter = indexWriter
+
+	return nil
 
 }
 
@@ -75,4 +132,10 @@ func (writer *Writer) Close() error {
 	}
 	return err2
 
+}
+
+var cdbSuffix = ".cdb"
+
+func newWriter(path, name string, counter int) (*cdb.Writer, error) {
+	return cdb.Create(path + pathSep + name + "_" + strconv.Itoa(counter) + cdbSuffix)
 }
--- writer_test.go
+++ writer_test.go
@@ -3,6 +3,7 @@ package vlcdb_test
 import (
 	"github.com/gnewton/vlcdb"
 	"log"
+	"strconv"
 	"testing"
 )
 
@@ -10,6 +11,28 @@ func TestWritesRandom(t *testing.T) {
 	_, err := vlcdb.Create("foo")
 	if err != nil {
 		log.Println(err)
+		log.Fatal(err)
+	}
+}
+
+func TestMany(t *testing.T) {
+	writer, err := vlcdb.Create("foo")
+	if err != nil {
+		log.Println(err)
+		log.Fatal(err)
+	}
+	for i := 0; i < 65855000; i++ {
+		si := strconv.Itoa(i)
+		//writer.Put([]byte("Alice"+si), []byte("Practice"))
+		err := writer.Put([]byte("Ai mmmmmnnnoocemmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"+si), []byte("foo nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn"+si))
+		if err != nil {
+			log.Println(si + " *************************")
+			log.Fatal(err)
+		}
+	}
+
+	err = writer.Close()
+	if err != nil {
 		log.Fatal(err)
 	}
 }
